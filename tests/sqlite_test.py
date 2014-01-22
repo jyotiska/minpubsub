e004f4f19c71aaa8ce905495aba6c7c70742d3ec
Jo <mitali@ubuntu.ubuntu-domain>
2014-01-22 23:49:17 +0530

    first commit

---
+++ tests/sqlite_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+class TestSQLitePubSub(unittest.TestCase):
+	def setUp(self):
+		from minpubsub import create
+		self.pubsub = create('sqlite')
+
+	def test_pubsub(self):
+		subscriber = self.pubsub.subscribe('channel')
+		self.pubsub.publish('channel', 'hello')
+		assert subscriber.getNext() == "hello"
+
+	def test_multi(self):
+		subscriber = self.pubsub.subscribe('channel_1', 'channel_2')
+		self.pubsub.publish('channel_1', 'hi')
+		self.pubsub.publish('channel_2', 'hola')
+		assert subscriber.getAll() == ['hi', 'hola']
+
+if __name__ == '__main__':
+	unittest.main()
